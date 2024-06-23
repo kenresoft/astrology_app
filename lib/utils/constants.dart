ad00187ce68b2cb6b3adab38bbdb6b0cf86fa961
kenresoft <amadikenneth8@gmail.com>
2024-06-23 23:22:58 +0100

    Added constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,36 @@
+class Constants {
+  const Constants._();
+
+  static final List<Map<String, String>> expandableItems = [
+    {
+      "title": "FAQ",
+      "details": "Your 1year career report is ready! download your career report......",
+      "time": "45 minutes ago",
+    },
+    {
+      "title": "Feedbacks & Support",
+      "details": "Details 2",
+      "time": "11:30 AM",
+    },
+    {
+      "title": "Terms & Conditions",
+      "details": "Details 3",
+      "time": "01:45 PM",
+    },
+    {
+      "title": "Privacy",
+      "details": "Details 4",
+      "time": "01:45 PM",
+    },
+    {
+      "title": "About US",
+      "details": "Details 4",
+      "time": "01:45 PM",
+    },
+    {
+      "title": "Contact US",
+      "details": "Details 5",
+      "time": "01:45 PM",
+    },
+  ];
+}
