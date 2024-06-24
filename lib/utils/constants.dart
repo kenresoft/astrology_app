787415b0bf2578953e4f2b457696fc900610a507
kenresoft <amadikenneth8@gmail.com>
2024-06-24 17:21:52 +0100

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -33,4 +33,69 @@ class Constants {
       "time": "01:45 PM",
     },
   ];
+
+  static final List<Map<String, String>> notificationItems = [
+    {
+      "title": "Astro",
+      "details": "Your 1year career report is ready! download your career report......",
+      "time": "45 minutes ago",
+      "icon": "assets/svg/astro.svg",
+      "counter": "1",
+    },
+    {
+      "title": "Events",
+      "details": "Details 2",
+      "time": "11:30 AM",
+      "icon": "assets/svg/events.svg",
+      "counter": "",
+    },
+    {
+      "title": "Roommates",
+      "details": "Details 3",
+      "time": "01:45 PM",
+      "icon": "assets/svg/roommates.svg",
+      "counter": "2",
+    },
+    {
+      "title": "Rentals",
+      "details": "Details 4",
+      "time": "01:45 PM",
+      "icon": "assets/svg/rentals.svg",
+      "counter": "",
+    },
+    {
+      "title": "Care Services",
+      "details": "Details 4",
+      "time": "01:45 PM",
+      "icon": "assets/svg/care_services.svg",
+      "counter": "",
+    },
+    {
+      "title": "IT Training & Placement",
+      "details": "Details 5",
+      "time": "01:45 PM",
+      "icon": "assets/svg/it_services.svg",
+      "counter": "",
+    },
+    {
+      "title": "Jobs",
+      "details": "Details 6",
+      "time": "01:45 PM",
+      "icon": "assets/img/jobs.png",
+      "counter": "",
+    },
+    {
+      "title": "Local Biz & Services",
+      "details": "Details 7",
+      "time": "01:45 PM",
+      "icon": "assets/svg/local_biz.svg",
+      "counter": "",
+    },
+  ];
+
+  static const String lorem = "Lorem ipsum dolor sit amet consectetur. Massa cras "
+      "in dictum tempus scelerisque elit. Lectus lectus morbi "
+      "aliquam nam faucibus vulputate. Tristique dictumst nunc "
+      "felis bibendum nunc facilisis in ullamcorper vivamus. "
+      "Sed venenatis sapien mattis vulputate tempor. Scelerisque.";
 }
